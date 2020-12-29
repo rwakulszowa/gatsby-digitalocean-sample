aec96452090581cef7d04513df536a2efd5967e8
rwakulszowa <rwakulszowa1@gmail.com>
2020-12-29 18:54:19 +0100

    idk

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,21 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 export default function Layout({ children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <h1> {data.site.siteMetadata.title} </h1>
       {children}
     </div>
   )
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,24 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-export default function About() {
+export default function About({ data }) {
   return (
     <Layout>
       <div style={{ color: `teal` }}>
-        <h1>About Gatsby</h1>
+        <h1>About {data.site.siteMetadata.title}</h1>
         <p>Some content.</p>
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
